94e8cef3f0cfcde7ab07d483e21ac625b967e81c
Ireedui0223 <90458436+Ireedui0223@users.noreply.github.com>
2022-04-13 13:20:14 +0800

    json file
    json file zagvarchilan hiisen  baigaa

---
+++ index.js
@@ -0,0 +1,48 @@
+{
+"news":[
+    {
+        "id" :1, 
+        "watched" : 1, 
+        "liked" : 1, 
+        "commented": 1, 
+        "title" :"",
+        "content" : {"header": [],
+                    "subheader":[],
+                    "content" : []
+                    },
+        "uploadData" : ""
+    },
+    {
+        "id" :2, 
+        "watched" : 1, 
+        "liked" : 1, 
+        "commented": 1, 
+        "title" :"",
+        "content" : {"header": [],
+                    "subheader":[],
+                    "content" : []
+                    },
+        "uploadData" : ""
+    },
+    {
+        "id" :3, 
+        "watched" : 1, 
+        "liked" : 1, 
+        "commented": 1, 
+        "title" :"",
+        "content" : {"header": [],
+                    "subheader":[],
+                    "content" : []
+                    },
+        "uploadData" : ""
+    },
+    {
+        "id" :4, 
+        "watched" : 1, 
+        "liked" : 1, 
+        "commented": 1, 
+        "title" :"",
+        "content" : "",
+        "uploadData" : ""
+    }
+]}
